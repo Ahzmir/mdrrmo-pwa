ddeb926234665c2d6a13b23802db29bf9abe46c9
pyowlo <piolo.melvin17@gmail.com>
2026-04-20 19:40:04 +0800

    Fixed Google Maps

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-52f2a342'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.pj1u0ghc57o"
+    "revision": "0.hd5i57oocdo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
